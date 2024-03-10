775cea8dfb10357b28528c2e47b42167be03e3f6
Ran Sabban <sabbanran@gmail.com>
2024-03-10 20:15:15 +0200

    Add book details & more data variables & conditional marks for book values

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,6 +1,12 @@
 export function BookPreview({book}){
+    function getPriceClass(){
+        if (book.listPrice.amount > 150) return 'price-red'
+        if (book.listPrice.amount < 20) return 'price-green'
+        else return ''
+    }
     return <article className="book-preview">
         <h2>{book.title}</h2>
-        <h5>{book.price}</h5>
+        <h5 className={getPriceClass()}>{book.listPrice.amount} {book.listPrice.currencyCode}</h5>
+        <img src={book.thumbnail}/>
     </article>
 }
--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -1,10 +1,33 @@
 export function BookDetails({book,onGoBack}){
+    function getPageCount(){
+        if (book.pageCount > 500) return 'Serious Reading'
+        if (book.pageCount > 200) return 'Descent Reading'
+        if (book.pageCount < 100) return 'Light Reading'
+    }
+
+    function getPublishDate(){
+        if (book.publishedDate < 2014) return '- Vintage'
+        if (book.publishedDate > 2023) return '- New'
+    }
+
+    function getPriceClass(){
+        if (book.listPrice.amount > 150) return 'price-red'
+        if (book.listPrice.amount < 20) return 'price-green'
+        else return ''
+    }
+
     return <section className="book-details">
         <button onClick={onGoBack}>Back</button>
         <h1>Book Title: {book.title}</h1>
-        <h5>Book Price: {book.price}</h5>
+        <h3>{book.subtitle}</h3>
+        <h5 className={getPriceClass()}>Book Price: {book.listPrice.amount+book.listPrice.currencyCode}</h5>
         <h2>Description:</h2>
         <p>{book.description}</p>
+        <h2>Authors: {book.authors.map(author => author + ' ')}</h2>
+        <h2>Book categories: {book.categories.map(category => category + ' ')}</h2>
+        <h2>LNG: {book.language}</h2>
+        <h3>Pages: {book.pageCount}<span> {getPageCount()}</span></h3>
+        <h3>Published Date {book.publishedDate} <span>{getPublishDate()}</span></h3>
         <img src={book.thumbnail}/>
     </section>
 }
