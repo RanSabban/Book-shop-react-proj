31a304007bdb86046062776d46d56613e9068e67
Ran Sabban <sabbanran@gmail.com>
2024-03-11 01:56:49 +0200

    Some design upgrades

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -3,11 +3,13 @@ import { BookPreview } from "./BookPreview.jsx"
 export function BookList({books,onSelectBook}){
 
     if (!books) return <div>LOADING</div>
-    return <ul className="book-list">
+    return <ul className="book-list clean-list">
         {
         books.map(book => 
-            <li key={book.id}>
+            <li key={book.id} className="book-card">
+                <div className="book-card-text">
                 <BookPreview book={book}/>
+                </div>
                 <div className="book-actions">
                     <button onClick={() => {onSelectBook(book)}}>Book Details</button>
                 </div>
